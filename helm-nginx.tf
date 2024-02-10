323c62a3ac5aebea6686fd24c9d8dfc1b604ec5a
Ariff Nazhan <58643675+oscarnz@users.noreply.github.com>
2024-02-10 22:46:52 +0800

    put oidc first before proceeding to helm

--- helm-nginx.tf
+++ helm-nginx.tf
@@ -1,17 +1,17 @@
-resource "kubernetes_namespace" "ingress-nginx" {
-  metadata {
-    name = "ingress-nginx"
-  }
-}
+# resource "kubernetes_namespace" "ingress-nginx" {
+#   metadata {
+#     name = "ingress-nginx"
+#   }
+# }
 
-resource "helm_release" "ingress-nginx" {
-    name       = "ingress-nginx"
-    chart      = "ingress-nginx"
-    repository = "https://kubernetes.github.io/ingress-nginx"
-    version    = "4.9.0"
-    namespace  = kubernetes_namespace.ingress-nginx.id
+# resource "helm_release" "ingress-nginx" {
+#     name       = "ingress-nginx"
+#     chart      = "ingress-nginx"
+#     repository = "https://kubernetes.github.io/ingress-nginx"
+#     version    = "4.9.0"
+#     namespace  = kubernetes_namespace.ingress-nginx.id
 
-    values = [templatefile("helm-nginx/values.yaml", {
+#     values = [templatefile("helm-nginx/values.yaml", {
        
-    })]
-}
+#     })]
+# }
