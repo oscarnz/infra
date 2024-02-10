aa14ce6db6f61507ff33e0129e871a3a39538cc7
Ariff Nazhan <58643675+oscarnz@users.noreply.github.com>
2024-02-11 01:02:58 +0800

    fix

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
-# terraform {
+terraform {
 
-#   cloud {
-#     organization = "nazhan-infra"
+  cloud {
+    organization = "nazhan-infra"
 
-#     workspaces {
-#       name = "infra"
-#     }
-#   }
+    workspaces {
+      name = "infra"
+    }
+  }
 
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.31.0"
-#     }
-#   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
 
-#   required_version = ">= 1.1.0"
-# }
+  required_version = ">= 1.1.0"
+}
 
-# provider "aws" {
-#   access_key = var.access_key
-#   secret_key = var.secret_key
-#   region     = var.region
-# }
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
 
 # data "aws_iam_policy_document" "assume_role" {
 #   statement {
