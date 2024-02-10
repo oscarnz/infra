6d699d066c8ce55b8153d76c21256a3a87aec50f
Ariff Nazhan <58643675+oscarnz@users.noreply.github.com>
2024-02-10 23:07:28 +0800

    change cluster name

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.deployment.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.deployment.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.deployment-3.token
+    token                  = data.aws_eks_cluster_auth.deployment.token
    }
 }
 
