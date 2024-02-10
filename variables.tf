a6b7893814370279fb174fbb4527ad723b8d3b3d
Ariff Nazhan <58643675+oscarnz@users.noreply.github.com>
2024-02-11 01:01:49 +0800

    remove

--- variables.tf
+++ variables.tf
@@ -1,27 +1,27 @@
-variable "access_key" {
-  description = "access_key"
-  type        = string
-}
+# variable "access_key" {
+#   description = "access_key"
+#   type        = string
+# }
 
-variable "secret_key" {
-  description = "secret_key"
-  type        = string
-}
+# variable "secret_key" {
+#   description = "secret_key"
+#   type        = string
+# }
 
-variable "region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
-}
+# variable "region" {
+#   description = "AWS Region"
+#   type        = string
+#   default     = "us-east-1"
+# }
 
-variable "subnet_id_1" {
-  description = "subnet id 1"
-  type        = string
-  default     = "subnet-05568fc56f5f0f344"
-}
+# variable "subnet_id_1" {
+#   description = "subnet id 1"
+#   type        = string
+#   default     = "subnet-05568fc56f5f0f344"
+# }
 
-variable "subnet_id_2" {
-  description = "subnet id 2"
-  type        = string
-  default     = "subnet-079fb83f0872fcdfa"
-}
+# variable "subnet_id_2" {
+#   description = "subnet id 2"
+#   type        = string
+#   default     = "subnet-079fb83f0872fcdfa"
+# }
